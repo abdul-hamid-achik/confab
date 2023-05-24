ef0fb92ef7879d2d003d62da9e16030694c006d6
abdul hamid <abdulachik@icloud.com>
2023-05-24 02:54:24 -0600

    forcing ssl in prod

--- lib/db.ts
+++ lib/db.ts
@@ -2,5 +2,5 @@ import { env } from '@/env.mjs';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const client = postgres(env.POSTGRES_URL);
+const client = postgres(process.env.NODE_ENV === 'production' ? `${env.POSTGRES_URL}&sslmode=require` : env.POSTGRES_URL);
 export const db = drizzle(client, {});
